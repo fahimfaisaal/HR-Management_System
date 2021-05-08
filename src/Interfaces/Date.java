172f616a4cedf62956debfbc37d77b025010e285
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-05-09 05:39:26 +0600

    modify all interfaces

--- src/Interfaces/Date.java
+++ src/Interfaces/Date.java
@@ -15,6 +15,4 @@ public interface Date {
 
 	int getYear();
 	void setYear(int year);
-
-	String toString();
 }
--- src/Interfaces/Human.java
+++ src/Interfaces/Human.java
@@ -2,6 +2,7 @@ package Interfaces;
 
 import Enums.Gender;
 import Classes.Date;
+import Enums.Relationship;
 
 public interface Human {
 	String getName();
@@ -11,15 +12,19 @@ public interface Human {
 	void setAge(int age);
 
 	Date getBirthDate();
-	void setBirthDate();
+	void setBirthDate(Date date);
 
 	Date getDeathDate();
-	void setDeathDate();
+	void setDeathDate(Date date);
 
 	String getLocation();
 	void setLocation(String location);
 
 	Gender getGender();
+	void setGender(Gender gender);
+
+	Relationship getRelationship();
+	void setRelationship(Relationship relationship);
 
 	String getProfession();
 	void setProfession(String profession);
