a5c03842481852cabbf80d4f0f90d07917d57c90
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-05-15 08:38:39 +0600

    add insttitue emnum

---
+++ src/Classes/Education/Education.java
@@ -0,0 +1,17 @@
+package Classes;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Education {
+	private boolean isGraduate = false;
+	private LinkedList<Certificate> certificates = new LinkedList<>()
+
+	private Scanner scan = new Scanner(System.in);
+
+
+
+	public void setEducationByUser() {
+
+	}
+}
---
+++ src/Enums/Institute.java
@@ -0,0 +1,8 @@
+package Enums;
+
+public enum Institute {
+	SCHOOL,
+	COLLEGE,
+	UNIVERSITY,
+	OTHER
+}
